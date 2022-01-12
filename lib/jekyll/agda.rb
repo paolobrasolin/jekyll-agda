08f1dd9d8b82b30d4ea2ae88423248c5311021f7
Paolo Brasolin <paolo.brasolin@gmail.com>
2022-01-12 15:17:02 +0100

    First commit

---
+++ lib/jekyll/agda.rb
@@ -0,0 +1,8 @@
+require "jekyll/agda/version"
+
+module Jekyll
+  module Agda
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/agda/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Agda
+    VERSION = "0.1.0"
+  end
+end
